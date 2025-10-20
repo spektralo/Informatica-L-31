9cb44d65570972190da52041af34e6e5d1592741
Spektralo <cviavattene11@gmail.com>
2025-10-20 22:39:28 +0200

    deleted Programmazione_II course folder

--- 1_anno/Programmazione_II/Programmazione_II/final/11_liste/2_inserimento_in_testa.cpp
+++
@@ -1,7 +0,0 @@
-
-
-
-void inserisci_in_testa(lista& inizio, Tipo_elem e) {
-   lista nuovo = new Nodo(e, inizio);   // genera nuovo Nodo
-   inizio = nuovo ;               // inizio punta il nuovo Nodo
-}
